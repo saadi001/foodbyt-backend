3b3625a75b63ef9846057eb091699f5ac6bef4dc
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-04-09 20:39:42 +0600

    verifyJWT middleware added

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+     const authHeader = req.headers.authorization;
+     if(!authHeader){
+          return res.status(401).send('unauthorized access')
+     }
+     const token = authHeader.split(' ')[1]
+     jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+          if(err){
+               return res.status(403).send({message: 'forbidden access'})
+          }
+          req.decoded = decoded
+          next()
+     })
+}
+
 async function run() {
      try {
           const itemsCollection = client.db('foodbyt').collection('items')
